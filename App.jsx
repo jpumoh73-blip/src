1eed3e7636f2259f06747cb1e3dcbeb06f828f76
Phinehas <jpumoh73@gmail.com>
2026-06-11 16:12:46 +0100

    todolist

--- App.jsx
+++ App.jsx
@@ -1,40 +1,10 @@
-import { useState } from "react";
+import TodoList from "./TodoList";
 
- 
-const productsData = [
-  {id:1,name:"Air Force Sneakers Shoe",price:120,image:"/images/shoe.jpg"},
-  {id:2,name:"Classic Hoodie",price:55,image:"/images/Classichoodie.jpg"},
-  {id:3,name:"Smart Watch",price:99,image:"/images/smartwatch.jpg"},
-  {id:4,name:"Wireless Headphones",price:80,images:"/images/wireless headphone.jpg"},
-  {id:5,name:"Nike T-Shirt",price:30,images:"/images/t-shirt.jpg"},
-]
-
-
-
-export default function App() { 
+ function App() {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>🛍️ My Pro Shop</h1>
-      
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        
-        {productsData.map(product => (
-          <div 
-            key={product.id} 
-            style={{ border: "1px solid #ddd", padding: "10px", width: "200px" }}
-          >
-            <img 
-              src={product.image} 
-              alt={product.name}
-              style={{ width: "100%", height: "180px", objectFit: "cover" }}
-            />
-            <h3>{product.name}</h3>
-            <p>${product.price}</p>
-            <button>Add to Cart</button>
-          </div>
-        ))}
-
-      </div>
+    <div>
+      <TodoList/>
     </div>
   )
-} 
+ }
+ export default App;
